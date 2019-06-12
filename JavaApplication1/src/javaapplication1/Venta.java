86b7272b07fc7f35cf4246577965ad122de8bc7a
Diego Beltran <46682433+DBProconty@users.noreply.github.com>
2019-06-12 01:15:25 -0500

    Factura View y Venta

---
+++ JavaApplication1/src/javaapplication1/Venta.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 
+ */
+public class Venta {
+    private String cod_entrega;
+    private Consecionario conse;
+    private String fecha;
+    private String estado;
+    private Usuario user;
+    private Pedido pedido;
+
+    public Venta() {
+    }
+
+    public String getCod_entrega() {
+        return cod_entrega;
+    }
+
+    public void setCod_entrega(String cod_entrega) {
+        this.cod_entrega = cod_entrega;
+    }
+
+    public Consecionario getConse() {
+        return conse;
+    }
+
+    public void setConse(Consecionario conse) {
+        this.conse = conse;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    
+    
+    
+}
